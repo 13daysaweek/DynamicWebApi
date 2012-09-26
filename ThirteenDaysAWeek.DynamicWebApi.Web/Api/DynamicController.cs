70ab7628e8ca817ae05556af80cc4a116c61f332
13daysaweek <chris.house.00@gmail.com>
2012-09-26 18:38:30 -0500

    Added code to dynamic controller

--- ThirteenDaysAWeek.DynamicWebApi.Web/Api/DynamicController.cs
+++ ThirteenDaysAWeek.DynamicWebApi.Web/Api/DynamicController.cs
@@ -4,10 +4,20 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Newtonsoft.Json.Linq;
 
 namespace ThirteenDaysAWeek.DynamicWebApi.Web.Api
 {
     public class DynamicController : ApiController
     {
+        public JObject Post([FromBody]JObject json)
+        {
+            dynamic newJson = json;
+
+            newJson.NewProperty = "Something";
+            newJson.Date = DateTime.Now;
+
+            return newJson;
+        }       
     }
 }
